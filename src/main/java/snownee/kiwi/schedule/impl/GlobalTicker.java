73755c145a377f94f3bf54618877bee7a7f01090
Snownee <1850986885@qq.com>
2019-11-18 11:13:49 +0800

    Side annotation

--- src/main/java/snownee/kiwi/schedule/impl/GlobalTicker.java
+++ src/main/java/snownee/kiwi/schedule/impl/GlobalTicker.java
@@ -1,5 +1,7 @@
 package snownee.kiwi.schedule.impl;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.TickEvent.Phase;
@@ -17,6 +19,7 @@ public enum GlobalTicker implements ITicker {
     }
 
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public static void onTickClient(TickEvent.ClientTickEvent event) {
         Scheduler.tick(event.phase == Phase.START ? PRE_CLIENT : POST_CLIENT);
     }
