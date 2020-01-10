3a1c18a3360f6685333fc3d3bc98268c281db6fe
Snownee <1850986885@qq.com>
2020-01-11 03:34:26 +0800

    Network rework

--- src/main/java/snownee/kiwi/network/Packet.java
+++ src/main/java/snownee/kiwi/network/Packet.java
@@ -6,24 +6,16 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.PacketDistributor.PacketTarget;
 
-public abstract class Packet
-{
-    public Packet()
-    {
+public abstract class Packet {
+    public Packet() {}
+
+    public void send(PacketTarget target) {
+        NetworkChannel.send(target, this);
     }
 
-    public void send(PacketTarget target)
-    {
-        NetworkChannel.INSTANCE.channel.send(target, this);
-    }
+    public void send() {}
 
-    public void send()
-    {
-
-    }
-
-    public static abstract class PacketHandler<T extends Packet>
-    {
+    public static abstract class PacketHandler<T extends Packet> {
         public abstract void encode(T msg, PacketBuffer buffer);
 
         public abstract T decode(PacketBuffer buffer);
