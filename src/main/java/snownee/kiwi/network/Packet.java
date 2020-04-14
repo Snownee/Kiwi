d670bd7ab218a0a1b6c251cd9dde34a254864090
Snownee <1850986885@qq.com>
2020-04-14 13:27:28 +0800

    Add helper method: Packet.send to player

--- src/main/java/snownee/kiwi/network/Packet.java
+++ src/main/java/snownee/kiwi/network/Packet.java
@@ -2,8 +2,10 @@ package snownee.kiwi.network;
 
 import java.util.function.Supplier;
 
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.network.PacketDistributor;
 import net.minecraftforge.fml.network.PacketDistributor.PacketTarget;
 
 public abstract class Packet {
@@ -11,6 +13,10 @@ public abstract class Packet {
 
     public void send(PacketTarget target) {
         NetworkChannel.send(target, this);
+    }
+
+    public void send(ServerPlayerEntity player) {
+        send(PacketDistributor.PLAYER.with(() -> player));
     }
 
     public void send() {}
