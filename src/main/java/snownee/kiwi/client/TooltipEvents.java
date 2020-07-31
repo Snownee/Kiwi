c95796e9c497117eb23ab6091069201e92c5bc5f
Snownee <1850986885@qq.com>
2020-08-01 05:43:00 +0800

    feat: copy item nbt

--- src/main/java/snownee/kiwi/client/TooltipEvents.java
+++ src/main/java/snownee/kiwi/client/TooltipEvents.java
@@ -5,14 +5,19 @@ import java.util.stream.Collectors;
 
 import com.google.common.base.Function;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,10 +25,12 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import snownee.kiwi.KiwiConfig;
 import snownee.kiwi.item.ModItem;
 
+@OnlyIn(Dist.CLIENT)
 @EventBusSubscriber(Dist.CLIENT)
 public final class TooltipEvents {
     private TooltipEvents() {}
 
+    private static ItemStack lastStack;
     private static CompoundNBT lastNBT;
     private static ITextComponent lastFormatted;
     private static Function<CompoundNBT, ITextComponent> formatter;
@@ -39,7 +46,23 @@ public final class TooltipEvents {
         if (!KiwiConfig.debugTooltip || !event.getFlags().isAdvanced()) {
             return;
         }
+
         ItemStack stack = event.getItemStack();
+        Minecraft minecraft = Minecraft.getInstance();
+        if (stack != lastStack && minecraft.player != null && InputMappings.isKeyDown(Minecraft.getInstance().getMainWindow().getHandle(), 292/*F3*/)) {
+            lastStack = stack;
+            CompoundNBT data = stack.getTag();
+            ITextComponent itextcomponent = new StringTextComponent(stack.getItem().getRegistryName().toString());
+            if (data != null) {
+                itextcomponent.appendSibling(data.toFormattedComponent());
+            }
+            itextcomponent.applyTextStyle(style -> {
+                style.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, itextcomponent.getString())).setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslationTextComponent("chat.copy.click"))).setInsertion(itextcomponent.getString());
+            });
+            minecraft.player.sendMessage(itextcomponent);
+            minecraft.gameSettings.showDebugInfo = !minecraft.gameSettings.showDebugInfo;
+        }
+
         List<ITextComponent> tooltip = event.getToolTip();
 
         if (Screen.hasShiftDown() && stack.hasTag()) {
