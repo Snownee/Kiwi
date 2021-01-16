2c47b6c7f8c4d980221b18f7e5cb535067383418
Snownee <mjy523@126.com>
2021-01-16 22:56:35 +0800

    update doc

--- src/main/java/snownee/kiwi/test/TestConfig.java
+++ src/main/java/snownee/kiwi/test/TestConfig.java
@@ -3,15 +3,14 @@ package snownee.kiwi.test;
 import java.util.Arrays;
 import java.util.List;
 
-import com.electronwill.nightconfig.core.conversion.Path;
-
 import net.minecraftforge.fml.config.ModConfig;
 import snownee.kiwi.config.KiwiConfig;
 import snownee.kiwi.config.KiwiConfig.Comment;
+import snownee.kiwi.config.KiwiConfig.Path;
 import snownee.kiwi.config.KiwiConfig.Range;
 import snownee.kiwi.config.KiwiConfig.WorldRestart;
 
-//@KiwiConfig("test")
+//@KiwiConfig(type = ModConfig.Type.COMMON)
 public class TestConfig {
 
     public static int intValue = 5;
