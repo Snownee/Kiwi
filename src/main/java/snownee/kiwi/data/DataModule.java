7c69e7fab48b62c04b2c9fcea2fc0e2bb99ef948
Snownee <mjy523@126.com>
2022-01-09 19:29:28 +0800

    fix: recipe things being registered too late

--- src/main/java/snownee/kiwi/data/DataModule.java
+++ src/main/java/snownee/kiwi/data/DataModule.java
@@ -4,7 +4,6 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import snownee.kiwi.AbstractModule;
 import snownee.kiwi.KiwiModule;
-import snownee.kiwi.loader.event.InitEvent;
 import snownee.kiwi.recipe.AlternativesIngredientSerializer;
 import snownee.kiwi.recipe.FullBlockIngredient;
 import snownee.kiwi.recipe.ModuleLoadedCondition;
@@ -16,12 +15,12 @@ import snownee.kiwi.recipe.crafting.RetextureRecipe;
 @KiwiModule("data")
 public final class DataModule extends AbstractModule {
 
-	public static final RecipeSerializer<NoContainersShapedRecipe> SHAPED_NO_CONTAINERS = new NoContainersShapedRecipe.Serializer();
-	public static final RecipeSerializer<NoContainersShapelessRecipe> SHAPELESS_NO_CONTAINERS = new NoContainersShapelessRecipe.Serializer();
-	public static final RecipeSerializer<RetextureRecipe> RETEXTURE = new RetextureRecipe.Serializer();
+	public static RecipeSerializer<NoContainersShapedRecipe> SHAPED_NO_CONTAINERS = new NoContainersShapedRecipe.Serializer();
+	public static RecipeSerializer<NoContainersShapelessRecipe> SHAPELESS_NO_CONTAINERS = new NoContainersShapelessRecipe.Serializer();
+	public static RecipeSerializer<RetextureRecipe> RETEXTURE = new RetextureRecipe.Serializer();
 
 	@Override
-	protected void init(InitEvent event) {
+	protected void preInit() {
 		CraftingHelper.register(ModuleLoadedCondition.Serializer.INSTANCE);
 		CraftingHelper.register(TryParseCondition.Serializer.INSTANCE);
 		CraftingHelper.register(RL("full_block"), FullBlockIngredient.SERIALIZER);
