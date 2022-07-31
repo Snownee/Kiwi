a3f3f6b101a4ee9544aec4221496e2531d144b2c
Snownee <i@snownee.com>
2022-07-31 16:56:24 +0800

    feat: implement ModuleLoadedCondition

--- src/main/java/snownee/kiwi/data/DataModule.java
+++ src/main/java/snownee/kiwi/data/DataModule.java
@@ -1,8 +1,10 @@
 package snownee.kiwi.data;
 
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import snownee.kiwi.AbstractModule;
 import snownee.kiwi.KiwiModule;
+import snownee.kiwi.recipe.ModuleLoadedCondition;
 import snownee.kiwi.recipe.crafting.NoContainersShapedRecipe;
 import snownee.kiwi.recipe.crafting.NoContainersShapelessRecipe;
 import snownee.kiwi.recipe.crafting.RetextureRecipe;
@@ -14,11 +16,8 @@ public final class DataModule extends AbstractModule {
 	public static final RecipeSerializer<NoContainersShapelessRecipe> SHAPELESS_NO_CONTAINERS = new NoContainersShapelessRecipe.Serializer();
 	public static final RecipeSerializer<RetextureRecipe> RETEXTURE = new RetextureRecipe.Serializer();
 
-	//	@Override
-	//	protected void preInit() {
-	//		CraftingHelper.register(ModuleLoadedCondition.Serializer.INSTANCE);
-	//		CraftingHelper.register(TryParseCondition.Serializer.INSTANCE);
-	//		CraftingHelper.register(RL("full_block"), FullBlockIngredient.SERIALIZER);
-	//		CraftingHelper.register(RL("alternatives"), AlternativesIngredientSerializer.INSTANCE);
-	//	}
+	@Override
+	protected void preInit() {
+		ResourceConditions.register(ModuleLoadedCondition.ID, ModuleLoadedCondition.INSTANCE);
+	}
 }
