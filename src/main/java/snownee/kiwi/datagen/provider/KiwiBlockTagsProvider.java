c04162c4ff5cd67de3b4262e1f1b821311346421
Snownee <i@snownee.com>
2023-02-04 05:52:12 +0800

    improve: tag datagen

--- src/main/java/snownee/kiwi/datagen/provider/KiwiBlockTagsProvider.java
+++ src/main/java/snownee/kiwi/datagen/provider/KiwiBlockTagsProvider.java
@@ -7,7 +7,6 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.BannerBlock;
 import net.minecraft.world.level.block.BaseRailBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ButtonBlock;
@@ -24,92 +23,95 @@ import net.minecraft.world.level.block.WallSignBlock;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import snownee.kiwi.Kiwi;
+import snownee.kiwi.datagen.provider.TagsProviderHelper.OptionalEntry;
 import snownee.kiwi.mixin.BlockAccess;
 
 public abstract class KiwiBlockTagsProvider extends BlockTagsProvider {
 
+	protected final TagsProviderHelper<Block> helper;
+
 	public KiwiBlockTagsProvider(PackOutput packOutput, CompletableFuture<Provider> lookupProvider, String modId, ExistingFileHelper existingFileHelper) {
 		super(packOutput, lookupProvider, modId, existingFileHelper);
+		helper = new TagsProviderHelper<>(this);
 	}
 
-	public void processTools(Block block) {
-		processTools(block, true);
+	public void processTools(OptionalEntry<Block> entry) {
+		processTools(entry, true);
 	}
 
-	public void processTools(Block block, boolean addParentInstead) {
-		if (block instanceof BannerBlock) {
-			tag(BlockTags.BANNERS).add(block);
-			return;
-		}
+	public boolean processTools(OptionalEntry<Block> entry, boolean addParentInstead) {
+		Block block = entry.object();
 		Material material = ((BlockAccess) block).getMaterial();
 		if (material == Material.LEAVES) {
-			tag(BlockTags.MINEABLE_WITH_HOE).add(block);
+			helper.add(BlockTags.MINEABLE_WITH_HOE, entry);
+			return true;
 		} else if (material == Material.WOOD || material == Material.NETHER_WOOD) {
 			if (addParentInstead) {
 				if (block instanceof WallSignBlock) {
-					tag(BlockTags.WALL_SIGNS).add(block);
-					return;
+					helper.add(BlockTags.WALL_SIGNS, entry);
+					return true;
 				}
 				if (block instanceof StandingSignBlock) {
-					tag(BlockTags.STANDING_SIGNS).add(block);
-					return;
+					helper.add(BlockTags.STANDING_SIGNS, entry);
+					return true;
 				}
 				if (block instanceof ButtonBlock) {
-					tag(BlockTags.WOODEN_BUTTONS).add(block);
-					return;
+					helper.add(BlockTags.WOODEN_BUTTONS, entry);
+					return true;
 				}
 				if (block instanceof DoorBlock) {
-					tag(BlockTags.WOODEN_DOORS).add(block);
-					return;
+					helper.add(BlockTags.WOODEN_DOORS, entry);
+					return true;
 				}
 				if (block instanceof FenceBlock) {
-					tag(BlockTags.WOODEN_FENCES).add(block);
-					return;
+					helper.add(BlockTags.WOODEN_FENCES, entry);
+					return true;
 				}
 				if (block instanceof StairBlock) {
-					tag(BlockTags.WOODEN_STAIRS).add(block);
-					return;
+					helper.add(BlockTags.WOODEN_STAIRS, entry);
+					return true;
 				}
 				if (block instanceof SlabBlock) {
-					tag(BlockTags.WOODEN_SLABS).add(block);
-					return;
+					helper.add(BlockTags.WOODEN_SLABS, entry);
+					return true;
 				}
 				if (block instanceof PressurePlateBlock) {
-					tag(BlockTags.WOODEN_PRESSURE_PLATES).add(block);
-					return;
+					helper.add(BlockTags.WOODEN_PRESSURE_PLATES, entry);
+					return true;
 				}
 				if (block instanceof TrapDoorBlock) {
-					tag(BlockTags.WOODEN_TRAPDOORS).add(block);
-					return;
+					helper.add(BlockTags.WOODEN_TRAPDOORS, entry);
+					return true;
 				}
 			}
-			tag(BlockTags.MINEABLE_WITH_AXE).add(block);
+			helper.add(BlockTags.MINEABLE_WITH_AXE, entry);
 		} else if (material == Material.PLANT || material == Material.REPLACEABLE_PLANT || material == Material.VEGETABLE) {
 			if (addParentInstead) {
 				if (block instanceof SaplingBlock) {
-					tag(BlockTags.SAPLINGS).add(block);
-					return;
+					helper.add(BlockTags.SAPLINGS, entry);
+					return true;
 				}
 			}
-			tag(BlockTags.MINEABLE_WITH_AXE).add(block);
+			helper.add(BlockTags.MINEABLE_WITH_AXE, entry);
+			return true;
 		} else if (material == Material.STONE || material == Material.METAL || material == Material.HEAVY_METAL) {
 			if (addParentInstead) {
 				if (block instanceof WallBlock) {
-					tag(BlockTags.WALLS).add(block);
-					return;
+					helper.add(BlockTags.WALLS, entry);
+					return true;
 				}
 				if (block instanceof BaseRailBlock) {
-					tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block);
-					return;
+					helper.add(BlockTags.RAILS, entry);
+					return true;
 				}
 			}
-			tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block);
+			helper.add(BlockTags.MINEABLE_WITH_PICKAXE, entry);
+			return true;
 		} else if (material == Material.SAND || material == Material.CLAY || material == Material.DIRT || material == Material.SNOW) {
-			tag(BlockTags.MINEABLE_WITH_SHOVEL).add(block);
-		} else {
-			Kiwi.logger.info("Unhandled block: {}", block);
+			helper.add(BlockTags.MINEABLE_WITH_SHOVEL, entry);
+			return true;
 		}
+		return false;
 	}
 
 	public TagsProvider.TagAppender<Block> tag(String name) {
