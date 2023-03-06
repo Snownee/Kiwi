e7af6d00490496440342b3c6ab0bfedf5194d3a6
Snownee <i@snownee.com>
2023-03-06 10:01:56 +0800

    shutup experimental settings

--- src/main/java/snownee/kiwi/mixin/client/WorldOpenFlowsMixin.java
+++ src/main/java/snownee/kiwi/mixin/client/WorldOpenFlowsMixin.java
@@ -3,10 +3,12 @@ package snownee.kiwi.mixin.client;
 import com.mojang.serialization.Lifecycle;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import net.minecraft.client.gui.screens.worldselection.WorldOpenFlows;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import snownee.kiwi.KiwiClientConfig;
 
 @Mixin(WorldOpenFlows.class)
-public class WorldOpenFlowsMixin {
+public abstract class WorldOpenFlowsMixin {
 
 	@Inject(method = "confirmWorldCreation", at = @At("HEAD"), cancellable = true)
 	private static void kiwi$confirmWorldCreation(Minecraft minecraft, CreateWorldScreen createWorldScreen, Lifecycle lifecycle, Runnable runnable, CallbackInfo ci) {
@@ -23,5 +25,16 @@ public class WorldOpenFlowsMixin {
 			ci.cancel();
 		}
 	}
+
+	@Inject(method = "loadLevel", at = @At("HEAD"), cancellable = true)
+	private void kiwi$loadLevel(Screen screen, String string, CallbackInfo ci) {
+		if (KiwiClientConfig.suppressExperimentalWarning) {
+			doLoadLevel(screen, string, false, false);
+			ci.cancel();
+		}
+	}
+
+	@Shadow
+	protected abstract void doLoadLevel(Screen lastScreen, String levelName, boolean safeMode, boolean checkAskForBackup);
 
 }
