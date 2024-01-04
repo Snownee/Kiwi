9adf08dce3840a2cb0c7174e5f2b014f12782657
Snownee <i@snownee.com>
2024-01-05 00:34:19 +0800

    improve: item category fallback strategy

--- src/main/java/snownee/kiwi/GroupSetting.java
+++ src/main/java/snownee/kiwi/GroupSetting.java
@@ -11,6 +11,8 @@ import com.google.common.collect.Lists;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.item.CreativeModeTab;
@@ -29,8 +31,9 @@ public class GroupSetting {
 		return new GroupSetting(category.value(), category.after());
 	}
 
-	private String[] groups;
-	private String[] after;
+	private final String[] groups;
+	private final String[] after;
+	private final List<ItemCategoryFiller> fillers = Lists.newArrayList();
 
 	public GroupSetting(String[] groups, String[] after) {
 		this.groups = groups;
@@ -38,22 +41,33 @@ public class GroupSetting {
 	}
 
 	public void apply(ItemCategoryFiller filler) {
-		/* off */
-		List<CreativeModeTab> tabs = Stream.of(groups)
-				.map(Kiwi::getGroup)
+		fillers.add(filler);
+	}
+
+	public void postApply() {
+		List<ResourceKey<CreativeModeTab>> tabKeys = Stream.of(groups)
+				.map($ -> {
+					CreativeModeTab tab = Kiwi.getGroup($);
+					if (tab != null) {
+						return BuiltInRegistries.CREATIVE_MODE_TAB.getResourceKey(tab).orElse(null);
+					}
+					return ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation($));
+				})
 				.filter(Objects::nonNull)
 				.toList();
-		Set<Item> afterItems = Stream.of(after)
-				.map(ResourceLocation::tryParse)
-				.filter(Objects::nonNull)
-				.map(BuiltInRegistries.ITEM::get)
-				.filter(Predicate.not(Items.AIR::equals))
-				.collect(Collectors.toSet());
-		/* on */
-		for (CreativeModeTab tab : tabs) {
-			ItemGroupEvents.modifyEntriesEvent(BuiltInRegistries.CREATIVE_MODE_TAB.getResourceKey(tab).orElseThrow()).register(entries -> {
+		for (ResourceKey<CreativeModeTab> tabKey : tabKeys) {
+			ItemGroupEvents.modifyEntriesEvent(tabKey).register(entries -> {
+				Set<Item> afterItems = Stream.of(after)
+						.map(ResourceLocation::tryParse)
+						.filter(Objects::nonNull)
+						.map(BuiltInRegistries.ITEM::get)
+						.filter(Predicate.not(Items.AIR::equals))
+						.collect(Collectors.toSet());
 				List<ItemStack> items = Lists.newArrayList();
-				filler.fillItemCategory(tab, entries.getEnabledFeatures(), entries.shouldShowOpRestrictedItems(), items);
+				for (ItemCategoryFiller filler : fillers) {
+					CreativeModeTab tab = BuiltInRegistries.CREATIVE_MODE_TAB.get(tabKey);
+					filler.fillItemCategory(tab, entries.getEnabledFeatures(), entries.shouldShowOpRestrictedItems(), items);
+				}
 				items = getEnabledStacks(items, entries.getEnabledFeatures());
 				addAfter(items, entries.getDisplayStacks(), afterItems);
 				addAfter(items, entries.getSearchTabStacks(), afterItems);
@@ -89,5 +103,9 @@ public class GroupSetting {
 
 	private static boolean isEnabled(ItemStack stack, FeatureFlagSet enabledFeatures) {
 		return stack.getItem().isEnabled(enabledFeatures);
+	}
+
+	public boolean isEmpty() {
+		return groups.length == 0 && after.length == 0;
 	}
 }
