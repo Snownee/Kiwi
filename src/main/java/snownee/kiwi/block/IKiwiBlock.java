f357ea7a0891ccb72796162892c983a19d038798
Snownee <i@snownee.com>
2024-05-23 14:05:45 +0800

    improve

--- src/main/java/snownee/kiwi/block/IKiwiBlock.java
+++ src/main/java/snownee/kiwi/block/IKiwiBlock.java
@@ -2,6 +2,7 @@ package snownee.kiwi.block;
 
 import org.jetbrains.annotations.Nullable;
 
+import net.fabricmc.fabric.api.block.BlockPickInteractionAware;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
@@ -9,13 +10,14 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.HitResult;
 import snownee.kiwi.item.ModBlockItem;
 
-public interface IKiwiBlock {
+public interface IKiwiBlock extends BlockPickInteractionAware {
 
 	default MutableComponent getName(ItemStack stack) {
 		return Component.translatable(stack.getDescriptionId());
@@ -32,5 +34,10 @@ public interface IKiwiBlock {
 			@Nullable Player player,
 			@Nullable HitResult hit) {
 		return ModBlock.pick(level, blockPos, blockState, player, hit);
+	}
+
+	@Override
+	default ItemStack getPickedStack(BlockState state, BlockGetter view, BlockPos pos, Player player, HitResult result) {
+		return getCloneItemStack(player.level(), pos, state, player, result);
 	}
 }
