0dc6a5c1b732bdbcd505ddd50d987fc06e44e6a8
SettingDust <settingdust@gmail.com>
2025-07-23 11:33:25 +0800

    refactor: use new network api for block entity

--- src/main/java/snownee/kiwi/block/entity/ModBlockEntity.java
+++ src/main/java/snownee/kiwi/block/entity/ModBlockEntity.java
@@ -7,9 +7,13 @@ import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.TagValueOutput;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 import snownee.kiwi.util.NotNullByDefault;
 
 /**
@@ -29,30 +33,31 @@ public abstract class ModBlockEntity extends BlockEntity {
 		return ClientboundBlockEntityDataPacket.create(this);
 	}
 
-	public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
-		readPacketData(pkt.getTag());
+	public void onDataPacket(Connection net, ValueInput valueInput) {
+		readPacketData(valueInput);
 	}
 
 	// Used for syncing data at the time when the chunk is loaded
 	@Override
 	public CompoundTag getUpdateTag(HolderLookup.Provider provider) {
-		return writePacketData(new CompoundTag(), provider);
+		TagValueOutput valueOutput = TagValueOutput.createWithContext(ProblemReporter.DISCARDING, provider);
+		writePacketData(valueOutput);
+		return valueOutput.buildResult();
 	}
 
 	/**
 	 * Read data for server-client syncing.
 	 *
-	 * @param data the data source
+	 * @param valueInput the data source
 	 */
-	protected abstract void readPacketData(CompoundTag data);
+	protected abstract void readPacketData(ValueInput valueInput);
 
 	/**
 	 * Write data for server-client syncing. ONLY write the necessary data!
 	 *
-	 * @param data the data sink
-	 * @return the parameter, or delegate to super method
+	 * @param valueOutput the data sink
 	 */
-	protected abstract CompoundTag writePacketData(CompoundTag data, HolderLookup.Provider provider);
+	protected abstract void writePacketData(ValueOutput valueOutput);
 
 	public void refresh() {
 		if (hasLevel() && !level.isClientSide) {
