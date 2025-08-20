609af879ee021a6fb81b5d417d705107e6b2af37
Snownee <i@snownee.com>
2025-08-20 19:30:16 +0800

    fix: always register builder's button

--- src/main/java/snownee/kiwi/customization/CustomizationClient.java
+++ src/main/java/snownee/kiwi/customization/CustomizationClient.java
@@ -40,10 +40,8 @@ import snownee.kiwi.Kiwi;
 import snownee.kiwi.RenderLayerEnum;
 import snownee.kiwi.customization.block.GlassType;
 import snownee.kiwi.customization.block.behavior.SitManager;
-import snownee.kiwi.customization.block.family.BlockFamilies;
 import snownee.kiwi.customization.block.loader.BlockDefinitionProperties;
 import snownee.kiwi.customization.block.loader.KBlockDefinition;
-import snownee.kiwi.customization.builder.BuilderRules;
 import snownee.kiwi.customization.builder.BuildersButton;
 import snownee.kiwi.customization.builder.ConvertScreen;
 import snownee.kiwi.customization.command.ExportBlocksCommand;
@@ -65,9 +63,6 @@ public final class CustomizationClient {
 	public static void init(IEventBus modEventBus) {
 		var forgeEventBus = NeoForge.EVENT_BUS;
 		modEventBus.addListener((RegisterKeyMappingsEvent event) -> {
-			if (!CustomizationHooks.kswitch && BlockFamilies.all().isEmpty() && BuilderRules.all().isEmpty()) {
-				return;
-			}
 			buildersButtonKey = new SmartKey.Builder("key.kiwi.builders_button", KeyMapping.CATEGORY_GAMEPLAY)
 					.key(InputConstants.getKey("key.mouse.4"))
 					.onLongPress(BuildersButton::onLongPress)
