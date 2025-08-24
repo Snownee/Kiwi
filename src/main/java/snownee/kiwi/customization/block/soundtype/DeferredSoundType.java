b489eeccbcb8a1e86b2bb408c141dde65efa79ce
SettingDust <settingdust@gmail.com>
2025-08-24 23:19:59 +0800

    feat: add data driven sound type

---
+++ src/main/java/snownee/kiwi/customization/block/soundtype/DeferredSoundType.java
@@ -0,0 +1,74 @@
+package snownee.kiwi.customization.block.soundtype;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.block.SoundType;
+import snownee.kiwi.util.DeferredHolder;
+
+public class DeferredSoundType extends SoundType {
+	private static final DeferredHolder<SoundEvent, SoundEvent> EMPTY_SOUND_EVENT = DeferredHolder.create(
+			Registries.SOUND_EVENT,
+			SoundEvents.EMPTY.getLocation());
+
+	public static final MapCodec<DeferredSoundType> DIRECT_CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+			Codec.FLOAT.optionalFieldOf("volume", 1.0f).forGetter(SoundType::getVolume),
+			Codec.FLOAT.optionalFieldOf("pitch", 1.0f).forGetter(SoundType::getPitch),
+			DeferredHolder.codec(Registries.SOUND_EVENT).optionalFieldOf("break_sound", EMPTY_SOUND_EVENT).forGetter(it -> it.breakSound),
+			DeferredHolder.codec(Registries.SOUND_EVENT).optionalFieldOf("step_sound", EMPTY_SOUND_EVENT).forGetter(it -> it.stepSound),
+			DeferredHolder.codec(Registries.SOUND_EVENT).optionalFieldOf("place_sound", EMPTY_SOUND_EVENT).forGetter(it -> it.placeSound),
+			DeferredHolder.codec(Registries.SOUND_EVENT).optionalFieldOf("hit_sound", EMPTY_SOUND_EVENT).forGetter(it -> it.hitSound),
+			DeferredHolder.codec(Registries.SOUND_EVENT).optionalFieldOf("fall_sound", EMPTY_SOUND_EVENT).forGetter(it -> it.fallSound)
+	).apply(instance, DeferredSoundType::new));
+
+	private final DeferredHolder<SoundEvent, SoundEvent> breakSound;
+	private final DeferredHolder<SoundEvent, SoundEvent> stepSound;
+	private final DeferredHolder<SoundEvent, SoundEvent> placeSound;
+	private final DeferredHolder<SoundEvent, SoundEvent> hitSound;
+	private final DeferredHolder<SoundEvent, SoundEvent> fallSound;
+
+	public DeferredSoundType(
+			float volumeIn,
+			float pitchIn,
+			DeferredHolder<SoundEvent, SoundEvent> breakSoundIn,
+			DeferredHolder<SoundEvent, SoundEvent> stepSoundIn,
+			DeferredHolder<SoundEvent, SoundEvent> placeSoundIn,
+			DeferredHolder<SoundEvent, SoundEvent> hitSoundIn,
+			DeferredHolder<SoundEvent, SoundEvent> fallSoundIn) {
+		super(volumeIn, pitchIn, null, null, null, null, null);
+		this.breakSound = breakSoundIn;
+		this.stepSound = stepSoundIn;
+		this.placeSound = placeSoundIn;
+		this.hitSound = hitSoundIn;
+		this.fallSound = fallSoundIn;
+	}
+
+	@Override
+	public SoundEvent getBreakSound() {
+		return breakSound.get();
+	}
+
+	@Override
+	public SoundEvent getStepSound() {
+		return stepSound.get();
+	}
+
+	@Override
+	public SoundEvent getPlaceSound() {
+		return placeSound.get();
+	}
+
+	@Override
+	public SoundEvent getHitSound() {
+		return hitSound.get();
+	}
+
+	@Override
+	public SoundEvent getFallSound() {
+		return fallSound.get();
+	}
+}
