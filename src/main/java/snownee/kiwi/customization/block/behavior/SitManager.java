e7701f79364a3c165a0a1844f88beac7057b3422
Snownee <i@snownee.com>
2025-10-08 22:29:41 +0800

    fix(ksit): player head rotation when sitting on stairs
    (cherry picked from commit d740bc4c98838d809983554938d5a459d39bb645)

--- src/main/java/snownee/kiwi/customization/block/behavior/SitManager.java
+++ src/main/java/snownee/kiwi/customization/block/behavior/SitManager.java
@@ -154,6 +154,7 @@ public class SitManager {
 				}
 			}
 			if (level.addFreshEntity(display)) {
+				rider.setYRot(display.getYRot());
 				rider.startRiding(display, true);
 				if (rider != player) {
 					((Mob) rider).dropLeash(true, true);
