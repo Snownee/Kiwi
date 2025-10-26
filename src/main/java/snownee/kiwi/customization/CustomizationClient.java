8aa11f614001c2539ecee50bd0be122185159d0c
Snownee <i@snownee.com>
2025-10-26 08:47:22 +0800

    feat: set builder's button default key to unknown (close #90)

--- src/main/java/snownee/kiwi/customization/CustomizationClient.java
+++ src/main/java/snownee/kiwi/customization/CustomizationClient.java
@@ -9,7 +9,6 @@ import org.jetbrains.annotations.Nullable;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.datafixers.util.Pair;
 
@@ -63,8 +62,7 @@ public final class CustomizationClient {
 	public static void init(IEventBus modEventBus) {
 		var forgeEventBus = NeoForge.EVENT_BUS;
 		modEventBus.addListener((RegisterKeyMappingsEvent event) -> {
-			buildersButtonKey = new SmartKey.Builder("key.kiwi.builders_button", KeyMapping.CATEGORY_GAMEPLAY)
-					.key(InputConstants.getKey("key.mouse.4"))
+			buildersButtonKey = new SmartKey.Builder("key.kiwi.builders_button2", KeyMapping.CATEGORY_GAMEPLAY)
 					.onLongPress(BuildersButton::onLongPress)
 					.onShortPress(BuildersButton::onShortPress)
 					.build();
