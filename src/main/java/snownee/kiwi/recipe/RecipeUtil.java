8012ec4ba77d3c9fded9b6440e2c5fdf3438453c
Snownee <i@snownee.com>
2026-01-27 19:22:10 +0800

    feat: RecipeUtil#tagIngredient now requires a HolderGetter parameter

--- src/main/java/snownee/kiwi/recipe/RecipeUtil.java
+++ src/main/java/snownee/kiwi/recipe/RecipeUtil.java
@@ -1,6 +1,6 @@
 package snownee.kiwi.recipe;
 
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -10,7 +10,7 @@ public final class RecipeUtil {
 		// Utility class
 	}
 
-	public static Ingredient tagIngredient(TagKey<Item> tag) {
-		return Ingredient.of(BuiltInRegistries.ITEM.get(tag).orElseThrow());
+	public static Ingredient tagIngredient(HolderGetter<Item> lookup, TagKey<Item> tag) {
+		return Ingredient.of(lookup.getOrThrow(tag));
 	}
 }
