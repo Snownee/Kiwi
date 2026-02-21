0a353e9384a00598fc69e01d2c113f66cd11bb3d
Snownee <i@snownee.com>
2026-02-21 10:52:46 +0800

    feat: EMI support for KSwitch groups

--- src/main/java/snownee/kiwi/customization/compat/emi/EMICompat.java
+++ src/main/java/snownee/kiwi/customization/compat/emi/EMICompat.java
@@ -7,8 +7,12 @@ import com.google.common.collect.Lists;
 import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.stack.EmiStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.StonecutterRecipe;
+import snownee.kiwi.Kiwi;
 import snownee.kiwi.customization.CustomizationHooks;
 import snownee.kiwi.customization.block.family.BlockFamilies;
 import snownee.kiwi.customization.block.family.BlockFamily;
@@ -17,9 +21,13 @@ import snownee.kiwi.util.KHolder;
 
 @EmiEntrypoint
 public class EMICompat implements EmiPlugin {
+	public static EmiRecipeCategory KSWITCH = new EmiRecipeCategory(Kiwi.id("kswitch"), EmiStack.of(Items.CHEST));
+
 	@Override
 	public void register(EmiRegistry registry) {
 		if (CustomizationHooks.isEnabled()) {
+			registry.addCategory(KSWITCH);
+
 			List<RecipeHolder<StonecutterRecipe>> recipes = Lists.newArrayList();
 			for (KHolder<BlockFamily> holder : BlockFamilies.all()) {
 				BlockFamily family = holder.value();
@@ -28,6 +36,9 @@ public class EMICompat implements EmiPlugin {
 				}
 				if (family.stonecutterExchange()) {
 					recipes.addAll(StonecutterRecipeMaker.makeRecipes("exchange_in_viewer", holder));
+				}
+				if (family.switchAttrs().enabled() && !family.itemHolders().isEmpty()) {
+					registry.addRecipe(new KSwitchGroupRecipe(holder));
 				}
 			}
 			for (RecipeHolder<StonecutterRecipe> recipe : recipes) {
---
+++ src/main/java/snownee/kiwi/customization/compat/emi/KSwitchGroupRecipe.java
@@ -0,0 +1,50 @@
+package snownee.kiwi.customization.compat.emi;
+
+import java.util.List;
+
+import dev.emi.emi.api.recipe.EmiIngredientRecipe;
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.recipe.EmiResolutionRecipe;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import net.minecraft.resources.ResourceLocation;
+import snownee.kiwi.customization.block.family.BlockFamily;
+import snownee.kiwi.util.KHolder;
+
+public class KSwitchGroupRecipe extends EmiIngredientRecipe {
+	private final EmiIngredient ingredient;
+	private final List<EmiStack> stacks;
+	private final ResourceLocation id;
+
+	public KSwitchGroupRecipe(KHolder<BlockFamily> family) {
+		ingredient = EmiIngredient.of(family.value().ingredientInViewer());
+		stacks = family.value().items().map(EmiStack::of).toList();
+		id = family.key().withPrefix("/");
+	}
+
+	@Override
+	protected EmiIngredient getIngredient() {
+		return ingredient;
+	}
+
+	@Override
+	protected List<EmiStack> getStacks() {
+		return stacks;
+	}
+
+	@Override
+	protected EmiRecipe getRecipeContext(EmiStack stack, int offset) {
+		return new EmiResolutionRecipe(ingredient, stack);
+	}
+
+	@Override
+	public EmiRecipeCategory getCategory() {
+		return EMICompat.KSWITCH;
+	}
+
+	@Override
+	public ResourceLocation getId() {
+		return id;
+	}
+}
