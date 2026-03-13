168ffa97c3a15c9e228586eee4fcb0febef37fc2
SettingDust <settingdust@gmail.com>
2026-03-13 14:48:51 +0800

    feat: use new render state

--- src/main/java/snownee/kiwi/mixin/customization/client/GameRendererMixin.java
+++ src/main/java/snownee/kiwi/mixin/customization/client/GameRendererMixin.java
@@ -8,23 +8,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.renderer.GameRenderer;
 import snownee.kiwi.customization.builder.ConvertScreen;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
 	@Inject(
-			method = "render",
+			method = "extractGui",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/client/Minecraft;getOverlay()Lnet/minecraft/client/gui/screens/Overlay;",
 					ordinal = 0))
 	private void kiwi$renderLingeringScreen(
 			DeltaTracker deltaTracker,
-			boolean renderLevel,
+			boolean shouldRenderLevel,
+			boolean resourcesLoaded,
 			CallbackInfo ci,
-			@Local(name = "graphics") GuiGraphics graphics) {
-		ConvertScreen.renderLingering(graphics);
+			@Local(name = "graphics") GuiGraphicsExtractor graphics) {
+		ConvertScreen.extractLingering(graphics);
 	}
 }
