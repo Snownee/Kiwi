9d053a8aea0ec94d715ee4e406045ce3f1ebb79f
SettingDust <settingdust@gmail.com>
2026-03-25 17:55:14 +0800

    feat: JEI support for KSwitch groups

---
+++ src/main/java/snownee/kiwi/customization/compat/jei/KSwitchGroupRecipe.java
@@ -0,0 +1,7 @@
+package snownee.kiwi.customization.compat.jei;
+
+import snownee.kiwi.customization.block.family.BlockFamily;
+import snownee.kiwi.util.KHolder;
+
+public record KSwitchGroupRecipe(KHolder<BlockFamily> family) {
+}
