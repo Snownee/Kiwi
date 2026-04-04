fe0817d4b61b6b2120bff4f61facadbf69ce7aa6
SettingDust <settingdust@gmail.com>
2026-04-05 01:54:38 +0800

    refactor(mixin): WorldOpenFlows use @WrapMethod

--- src/main/java/snownee/kiwi/mixin/client/WorldOpenFlowsMixin.java
+++ src/main/java/snownee/kiwi/mixin/client/WorldOpenFlowsMixin.java
@@ -3,32 +3,40 @@ package snownee.kiwi.mixin.client;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.mojang.serialization.Lifecycle;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import net.minecraft.client.gui.screens.worldselection.WorldOpenFlows;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import snownee.kiwi.KiwiClientConfig;
 
 @Mixin(WorldOpenFlows.class)
 public abstract class WorldOpenFlowsMixin {
-	@ModifyVariable(method = "confirmWorldCreation", at = @At("HEAD"), argsOnly = true)
-	private static boolean kiwi$confirmWorldCreation(boolean value) {
-		if (KiwiClientConfig.suppressExperimentalWarning) {
-			return true;
-		}
-		return value;
+	@WrapMethod(method = "confirmWorldCreation")
+	private static void kiwi$confirmWorldCreation(
+			Minecraft minecraft,
+			CreateWorldScreen parent,
+			Lifecycle lifecycle,
+			Runnable task,
+			boolean skipWarning,
+			Operation<Void> original) {
+		original.call(minecraft, parent, lifecycle, task, KiwiClientConfig.suppressExperimentalWarning || skipWarning);
 	}
 
 	@Inject(method = "askForBackup", at = @At("HEAD"), cancellable = true)
 	private void kiwi$askForBackup(
-			LevelStorageSource.LevelStorageAccess levelStorageAccess,
-			boolean isOldCustomizedWorld,
-			Runnable runnable,
-			Runnable runnable2,
+			LevelStorageSource.LevelStorageAccess worldAccess,
+			boolean oldCustomized,
+			Runnable proceedCallback,
+			Runnable cancelCallback,
 			CallbackInfo ci) {
-		if (KiwiClientConfig.suppressExperimentalWarning && !isOldCustomizedWorld) {
-			runnable.run();
+		if (KiwiClientConfig.suppressExperimentalWarning && !oldCustomized) {
+			proceedCallback.run();
 			ci.cancel();
 		}
 	}
