1afd9b2eea9163d9488e0f8a57f8d21cecd19f9c
SettingDust <settingdust@gmail.com>
2026-04-05 11:12:41 +0800

    refactor(datagen): drop fabric-only generator hooks

--- src/main/java/snownee/kiwi/datagen/KiwiBlockLoot.java
+++ src/main/java/snownee/kiwi/datagen/KiwiBlockLoot.java
@@ -10,36 +10,33 @@ import java.util.function.Function;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.LootTable.Builder;
 import snownee.kiwi.KiwiModuleContainer;
 import snownee.kiwi.KiwiModules;
 
-public abstract class KiwiBlockLoot extends FabricBlockLootTableProvider {
+public abstract class KiwiBlockLoot extends BlockLootSubProvider {
 	protected final Identifier moduleId;
 	private final List<Block> knownBlocks;
 	private final Map<Class<?>, Function<Block, LootTable.Builder>> handlers = Maps.newIdentityHashMap();
 	private final Set<Block> added = Sets.newHashSet();
 	private Function<Block, LootTable.Builder> defaultHandler;
 
-	protected KiwiBlockLoot(
-			Identifier moduleId,
-			FabricDataOutput dataOutput,
-			CompletableFuture<HolderLookup.Provider> registryLookup) {
-		super(dataOutput, registryLookup);
+	protected KiwiBlockLoot(Identifier moduleId, CompletableFuture<HolderLookup.Provider> registryLookup) {
+		super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registryLookup.join());
 		this.moduleId = moduleId;
 		KiwiModuleContainer container = Objects.requireNonNull(KiwiModules.get(moduleId));
 		knownBlocks = container.getRegistries(Registries.BLOCK);
 	}
 
+	@SuppressWarnings("unchecked")
 	protected <T extends Block> void handle(Class<T> clazz, Function<T, LootTable.Builder> handler) {
-		handlers.put(clazz, (Function<Block, Builder>) handler);
+		handlers.put(clazz, (Function<Block, LootTable.Builder>) handler);
 	}
 
 	protected void handleDefault(Function<Block, LootTable.Builder> handler) {
@@ -47,7 +44,7 @@ public abstract class KiwiBlockLoot extends FabricBlockLootTableProvider {
 	}
 
 	@Override
-	public void generate() {
+	protected void generate() {
 		addTables();
 		for (Block block : knownBlocks) {
 			if (added.contains(block)) {
@@ -70,18 +67,13 @@ public abstract class KiwiBlockLoot extends FabricBlockLootTableProvider {
 	protected abstract void addTables();
 
 	@Override
-	public void add(Block block, LootTable.Builder builder) {
+	protected void add(Block block, LootTable.Builder builder) {
 		super.add(block, builder);
 		added.add(block);
 	}
 
 	@Override
-	public List<Block> getKnownBlocks() {
+	protected Iterable<Block> getKnownBlocks() {
 		return knownBlocks;
-	}
-
-	@Override
-	public String getName() {
-		return super.getName() + " - " + moduleId;
 	}
 }
--- src/main/java/snownee/kiwi/datagen/KiwiDataGen.java
+++ src/main/java/snownee/kiwi/datagen/KiwiDataGen.java
@@ -1,24 +1,19 @@
 package snownee.kiwi.datagen;
 
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.fabric.impl.datagen.FabricDataGenHelper;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import snownee.kiwi.Kiwi;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
-public class KiwiDataGen implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		pack.addProvider(KiwiLanguageProvider::new);
-	}
-
+@EventBusSubscriber(modid = Kiwi.ID, bus = EventBusSubscriber.Bus.MOD)
+public class KiwiDataGen {
 	@SubscribeEvent
 	public static void on(GatherDataEvent event) {
-		//noinspection UnstableApiUsage
-		FabricDataGenHelper.runDatagenForMod(Kiwi.ID, Kiwi.ID, new KiwiDataGen(), event);
+		if (!event.getMods().contains(Kiwi.ID)) {
+			return;
+		}
+		event.getGenerator().addProvider(
+				event.includeClient(),
+				new KiwiLanguageProvider(event.getGenerator().getPackOutput(), Kiwi.ID, event.getLookupProvider()));
 	}
 }
