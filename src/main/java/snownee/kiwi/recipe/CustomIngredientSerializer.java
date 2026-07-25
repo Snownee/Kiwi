4babd059b0dce897ff6fc5b31477dac5c821abdc
Snownee <i@snownee.com>
2026-07-26 07:55:10 +0800

    .

--- src/main/java/snownee/kiwi/recipe/CustomIngredientSerializer.java
+++ src/main/java/snownee/kiwi/recipe/CustomIngredientSerializer.java
@@ -1,4 +1,18 @@
 package snownee.kiwi.recipe;
 
+import org.jspecify.annotations.Nullable;
+
+import net.minecraft.resources.Identifier;
+
 public interface CustomIngredientSerializer<T extends CustomIngredient> extends net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer<T> {
+	static void register(CustomIngredientSerializer<?> serializer) {
+		net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer.register(serializer);
+	}
+
+	@Nullable
+	static CustomIngredientSerializer<?> get(Identifier identifier) {
+		net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer<?> serializer = net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer.get(
+				identifier);
+		return serializer instanceof CustomIngredientSerializer ? (CustomIngredientSerializer<?>) serializer : null;
+	}
 }
